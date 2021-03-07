8af8cf555463ed95f6fc27634f17dcc7e58f0d50
eric <lee.eric.03@gmail.com>
2021-03-06 21:11:58 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+document.querySelector('main').remove()
+
+const newHeader = document.createElement('h1');
+newHeader.id = ('victory');
+newHeader.innerHTML = "<h1>Eric Lee is the champion</h1>";
+document.body.appendChild(newHeader);
+
+ 
+
+    
+
+
+
